6f840b2f41aaab54b2b143d6aa9ab8f433790172
zombiedr82 <820305@mystma.org>
2026-05-08 14:04:25 -0500

    update

---
+++ JavascriptStuff/LogIn/scripts.js
@@ -0,0 +1,12 @@
+function checkUsername(){
+   if(document.getElementById("username").value == "zoombiedrink"){
+      Access Granted.innerHTML
+   }
+}
+function checkPassword(){
+   if(document.getElementById("password").value == "1221Chiron"){
+      Access Denied.innerHTML
+   }
+}
+document.getElementById("username").value;
+document.getElementById("password").value;
--- JavascriptStuff/lootbox/scripts.js
+++ JavascriptStuff/lootbox/scripts.js
@@ -1,29 +1,54 @@
-var lootBox = document.getElementById("lootbox")
-var count_text =document.getElementById("common")
-var count_text =document.getElementById("uncommon")
+var lootBox = document.getElementById("lootbox");
+
+var common = document.getElementById("common");
+var c_count = 0;
+
+var uncommon = document.getElementById("uncommon");
+var u_count = 0;
+
+var rare = document.getElementById("rare");
+var r_count = 0;
+
+var legendary = document.getElementById("legendary");
+var l_count = 0;
+
+var mythic = document.getElementById("mythic");
+var m_count = 0;
+
+
 function rollLootbox(){
    number = Math.floor(Math.random() * 100);
 
    if (number < 50) {
       lootBox.innerHTML = "COMMON";
       lootBox.style.backgroundColor = "white";
+      c_count = c_count + 1;
+      common.innerHTML = c_count;
    }
 
    else if (number < 75) {
       lootBox.innerHTML = "UNCOMMON";
       lootBox.style.backgroundColor = "blue";
+      u_count = u_count + 1;
+      uncommon.innerHTML = u_count;
    }
    else if (number < 90) {
       lootBox.innerHTML = "RARE";
       lootBox.style.backgroundColor = "gold";
+      r_count = r_count + 1;
+      rare.innerHTML = r_count;
    }
    else if (number < 99) {
       lootBox.innerHTML = "LEGENDARY";
       lootBox.style.backgroundColor = "orangered";
+      l_count = l_count + 1;
+      legendary.innerHTML = l_count;
    }
    else {
       lootBox.innerHTML = "MYTHIC";
       lootBox.style.backgroundColor = "pink";
+      m_count = m_count + 1;
+      mythic.innerHTML = m_count;
    }
 }
 function plusOne(){
