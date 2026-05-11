7dccead9488763a7e9239451653d03de65af07ba
zombiedr82 <820305@mystma.org>
2026-05-11 14:06:08 -0500

    update

--- JavascriptStuff/LogIn/scripts.js
+++ JavascriptStuff/LogIn/scripts.js
@@ -1,12 +1,24 @@
-function checkUsername(){
-   if(document.getElementById("username").value == "zoombiedrink"){
-      Access Granted.innerHTML
+username_entry = document.getElementById("username-entry");
+password_entry = document.getElementById("password-entry");
+submit_button = document.getElementById("submit-button");
+status_text = document.getElementById("status-text");
+
+
+
+
+function checkCredentials(){
+
+   if (username_entry.value == "admin" && password_entry.value == "admin"){
+      status_text.innerHTML = "ACCESS GRANTED";
+      status_text.style.color = "green";
    }
-}
-function checkPassword(){
-   if(document.getElementById("password").value == "1221Chiron"){
-      Access Denied.innerHTML
+
+      else {
+         status_text.innerHTML = "ACCESS DENIED Incorrect Username or password";
+         status_text.style.color = "red";
+      }
+
    }
-}
-document.getElementById("username").value;
-document.getElementById("password").value;
+submit_button.addEventListener("click", checkCredentials)
+
+if(document.getElementById("username").value == "zoombiedrink" && document.getElementById("password").value == "1221Chiron"){
