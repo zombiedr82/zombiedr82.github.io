a61191a4867b16b3e37197f96386973f58838ddf
zombiedr82 <820305@mystma.org>
2026-05-13 14:13:07 -0500

    update

--- JavascriptStuff/BuzzfeedQuiz/scripts.js
+++ JavascriptStuff/BuzzfeedQuiz/scripts.js
@@ -36,23 +36,63 @@ var score_Troy = 0;
 function check_q1(){
 
    if (q1_a1.checked == true){
-      score_ = score _ + 1;
-      result.innerHTML = "You chose answer 1";
+      score_scott = score_scott + 1;
    }
    else if (q1_a2.checked == true){
-      score_ = score _ + 1;
-      result.innerHTML = "You chose answer 2";
+      score_Ilya = score_Ilya + 1;
+      score_Shane = score_Shan + 1;
+      score_Bood = score_Bood + 1;
+      score_Wyatt = score_Wyatt + 1;
+      score_Harris = score_Harris + 1;
+      score_Chiron = score_Chiron + 1;
+      score_Anya = score_Anya + 1;
+      score_Troy = score_Troy + 1;
    }
    else if (q1_a3.checked == true){ 
-      score_ = score _ + 1;
-      result.innerHTML = "You chose answer 3";
+      score_Shane = score_Shane + 1;
+      score_Hayden = score_Hayden + 1;
+      score_JJ = score_JJ + 1;
    }
    else if (q1_a4.checked == true){
-      score_ = score _ + 1;
-      result.innerHTML = "You didn't choose anything"
-   }
+      score_Ilya = score_Ilya + 1;
+      score_Cliff = score_Cliff + 1;
 }
 function check_q2(){
+   if (q1_a1.checked ==true){
+      
+   }
+}
+function check_q3(){
+   
+}
+function check_q4(){
+   
+}
+function check_q5(){
+   
+}
+function check_q6(){
+   
+}
+function check_q7(){
+   
+}
+function check_q8(){
+   
+}
+function check_q9(){
+   
+}
+function check_q10(){
+   
+}
+function check_q11(){
+   
+}
+function check_q12(){
+   
+}
+function check_q13(){
    
 }
 
